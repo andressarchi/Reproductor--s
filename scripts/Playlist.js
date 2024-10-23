dcf178ab544a3116998653fd019c1f6925d3f077
andressarchi <andresfelipesarchivalencia@gmail.com>
2024-10-23 09:33:50 -0500

    terminacion de codigo

--- scripts/Playlist.js
+++ scripts/Playlist.js
@@ -2,31 +2,31 @@ export const playList = [
     {
         title: 'Just a lil',
         artist: '50 Cent',
-        img: '/assets/images/just a lil.jpg', // Asegúrate de que esta ruta sea correcta
-        song: '/assets/audio/50_Cent_Just_A_Lil_Bit.mp3' // Asegúrate de que esta ruta sea correcta
+        img: '/assets/images/justalil.jpg', // Asegúrate de que esta ruta sea correcta
+        song: '/assets/audio/50CentJustALilBit.mp3' // Asegúrate de que esta ruta sea correcta
     },
     {
         title: 'Degenere',
         artist: 'Myke Tower',
         img: '/assets/images/degenere.jpg',
-        song: '/assets/audio/Myke Towers.mp3'
+        song: '/assets/audio/MykeTowers.mp3'
     },
     {
         title: 'Regalame una noche ',
         artist: 'Maelo Ruiz',
-        img: '/assets/images/regalame una noche.jfif',
-        song: '/assets/audio/Regalame Una Noche  Maelo Ruiz.mp3'
+        img: '/assets/images/regalameunanoche.jfif',
+        song: '/assets/audio/regalameunanoche.mp3'
     },
     {
         title: 'Me rehuso',
         artist: 'Dany ocean',
-        img: '/assets/images/me rehuso.jfif',
-        song: '/assets/audio/Danny Ocean   Me Rehúso.mp3'
+        img: '/assets/images/merehuso.jfif',
+        song: '/assets/audio/Merehuso.mp3'
     },
     {
         title: 'Gata G',
         artist: 'Ryan castro',
-        img: '/assets/images/gata g.jfif',
-        song: '/assets/audio/GATA G ryan castro.mp3'
+        img: '/assets/images/gatag.jfif',
+        song: '/assets/audio/gatag.mp3'
     },
 ];
